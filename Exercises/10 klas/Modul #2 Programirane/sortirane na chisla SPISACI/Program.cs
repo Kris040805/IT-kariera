ce00c2fc50d6ca32ddd95700c95c5046f36bcadd
Kris040805 <113238706+Kris040805@users.noreply.github.com>
2023-01-18 21:14:24 +0200

    Add files via upload

---
+++ Exercises/10 klas/Modul #2 Programirane/sortirane na chisla SPISACI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace sortirane_na_chisla_SPISACI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> nums = Console.ReadLine().Split().Select(double.Parse).ToList();
+            nums.Sort();
+            Console.WriteLine(string.Join(" <= ", nums));
+        }
+    }
+}
---
+++ Exercises/10 klas/Modul #2 Programirane/sortirane/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace sortirane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < a.Length; j++)
+                {
+                    if (a[i] < a[j])
+                    {
+                        int temp = a[i];
+                        a[i] = a[j]; ;
+                        a[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", a));
+        }
+    }
+}
