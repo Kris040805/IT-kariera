dda542431512e78c706e658d4720d8a40b51962d
Kris040805 <113238706+Kris040805@users.noreply.github.com>
2023-01-18 17:06:36 +0200

    Add files via upload

---
+++ Exams/Exam module №1/PostavqnеNaLaminat/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PostavqnaNaLaminat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceLaminate = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double lenght = double.Parse(Console.ReadLine());
+            double pricePad = double.Parse(Console.ReadLine());
+
+            double roomArea = lenght * width;
+            double TotalLamPrice = roomArea * priceLaminate;
+            double TotalPadPrice = roomArea * pricePad;
+            double work = 0.4 * (TotalLamPrice + TotalPadPrice);
+            double allPrice = TotalPadPrice + TotalLamPrice + work;
+            Console.WriteLine(Math.Round((allPrice),2));
+        }
+    }
+}
