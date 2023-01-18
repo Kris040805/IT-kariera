97c899dc0b4f3c06d4a2d6df18010cf0b77b3836
Kris040805 <113238706+Kris040805@users.noreply.github.com>
2023-01-18 17:11:12 +0200

    Add files via upload

---
+++ Exams/Exam module #2/passStatistics/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace passStatistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, int> playerStats = new SortedDictionary<string, int>();
+            
+            string currentPlayer = Console.ReadLine();
+            while (currentPlayer != "End of match")
+            {
+                string[] playerPass = currentPlayer.Split(" - ").ToArray();
+                string playerName = playerPass[0];
+                int playerPasses = int.Parse(playerPass[1]);
+
+                if (!playerStats.ContainsKey(playerName))
+                {
+                    playerStats.Add(playerName, playerPasses);
+                }
+                else
+                {
+                    playerStats[playerName] += playerPasses;
+                }
+                
+                currentPlayer = Console.ReadLine();
+            }
+
+            foreach (var pair in playerStats)
+            {
+                Console.WriteLine($"{pair.Key} has passed {pair.Value} passes.");
+            }
+        }
+    }
+}
