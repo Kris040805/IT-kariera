1018946e3c485525fa54ef38ff93460628ea5269
Kris040805 <113238706+Kris040805@users.noreply.github.com>
2023-01-18 21:11:13 +0200

    Add files via upload

---
+++ Exercises/10 klas/Modul #2 Programirane/broi na realni chisla/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace broi_na_realni_chisla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            var counts = new SortedDictionary<double, int>();
+
+            foreach (var key in nums)
+            {
+                if (counts.ContainsKey(key))
+                {
+                    counts[key]++;
+                }
+                else
+                {
+                    counts[key] = 1;
+                }
+            }
+
+            foreach (var key in counts.Keys)
+            {
+                Console.WriteLine("{0} -> {1}", key, counts[key]);
+            }
+        }
+    }
+}
---
+++ Exercises/10 klas/Modul #2 Programirane/cenzora/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace cenzora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banWords = Console.ReadLine().Split();
+            string sen = Console.ReadLine();
+
+            foreach (var banWord in banWords)
+            {
+                if (sen.Contains(banWord))
+                {
+                    sen = sen.Replace(banWord, new String('*', banWord.Length));
+                }
+            }
+            
+            Console.WriteLine(sen);
+        }
+    }
+}
