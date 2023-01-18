3699b334628d1cd91b01e26ef3651bfabbd6ed73
Kris040805 <113238706+Kris040805@users.noreply.github.com>
2023-01-18 20:50:03 +0200

    Add files via upload

---
+++ Exercises/10 klas/Modul #1 Uvod v programiraneto/sumaNaChisla/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace delenieBezOstatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int Sum = 0;
+            int max = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int a= int.Parse(Console.ReadLine());
+                Sum = Sum + a;
+                if (a > max)
+                {
+                    max = a;
+                }
+                
+            }
+            
+            Sum = Sum - max;
+            if (Sum == max)
+            {
+                Console.WriteLine("Sum = " + max);
+            }
+            else
+            {
+                Console.WriteLine("Diff = " + Math.Abs(max-Sum));
+            }
+        }
+    }
+}
---
+++ Exercises/10 klas/Modul #1 Uvod v programiraneto/transportPrice/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace transportPrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string dayTime = Console.ReadLine();
+            double taxiPrice = 0.90;
+
+            if (dayTime == "day")
+            {
+                taxiPrice = 0.79;
+            }
+
+            if (n >= 100)
+            {
+                double totalPrice = n * 0.06;
+                Console.WriteLine($"{totalPrice:F2}");
+            }
+            else if (n >= 20)
+            {
+                double totalPrice = n * 0.09;
+                Console.WriteLine($"{totalPrice:F2}");
+            }
+            else
+            {
+                double totalPrice = 0.70 + n * taxiPrice;
+                Console.WriteLine($"{totalPrice:F2}");
+            }
+
+        }
+    }
+}
