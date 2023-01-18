0d8e52112cf33e2ad556f920961bda983455d51e
Kris040805 <113238706+Kris040805@users.noreply.github.com>
2023-01-18 17:10:30 +0200

    Add files via upload

---
+++ Exams/Exam module #2/nameGame/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nameGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+
+            string currentName = Console.ReadLine();
+            while (currentName != "END OF GAME")
+            {
+                names.Add(currentName);
+                currentName = Console.ReadLine();
+            }
+
+            int max = 0;
+            string winnerName = "";
+
+            foreach (var name in names)
+            {
+                int pointsOfCurrentName = 0;
+
+                if (name.EndsWith('a'))
+                {
+                    pointsOfCurrentName += 10;
+                }
+                if (name.EndsWith('v'))
+                {
+                    pointsOfCurrentName += 13;
+                }
+                if (name.Length >= 7)
+                {
+                    pointsOfCurrentName += 33;
+                }
+                else
+                {
+                    pointsOfCurrentName += 22;
+                }
+
+                if (pointsOfCurrentName > max)
+                {
+                    max = pointsOfCurrentName;
+                    winnerName = name;
+                }
+            }
+            Console.WriteLine($"Winner is name: {winnerName}");
+            Console.WriteLine($"Points: {max}");
+        }
+    }
+}
