8c8fa561818fdb3e6f8438e8d4d055ca8dc84782
Kris040805 <113238706+Kris040805@users.noreply.github.com>
2023-01-18 17:01:07 +0200

    Add files via upload

---
+++ Exams/Exam module №1/ChristmasPresents/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ChristmasPresents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            double pres1 = double.Parse(Console.ReadLine());
+
+            double pres2 = 0.2 * pres1;
+            double pres3 = 0.7 * pres2;
+            double pres4 = 0.5 * pres2;
+
+            double TotalPrice = pres1 + pres2 + pres3 + pres4;
+
+            if (TotalPrice <= budget)
+            {
+                Console.WriteLine($"Yes! {budget - TotalPrice:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"No! {TotalPrice - budget:f2} leva needed.");
+            }
+        }
+    }
+}
