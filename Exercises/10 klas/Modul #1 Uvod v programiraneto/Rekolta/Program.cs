b2ac5cbf4c8cdca63d4e5c3d59b40abfea3c7cbf
Kris040805 <113238706+Kris040805@users.noreply.github.com>
2023-01-18 20:53:12 +0200

    Add files via upload

---
+++ Exercises/10 klas/Modul #1 Uvod v programiraneto/Rekolta/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Rekolta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int area = int.Parse(Console.ReadLine());
+            double grapePERsquare = double.Parse(Console.ReadLine());
+            int wineNeeded = int.Parse(Console.ReadLine());
+            int workers = int.Parse(Console.ReadLine());
+
+            double grape = area * grapePERsquare;
+            double wine = 0.4 * grape / 2.5;
+
+            if (wine < wineNeeded)
+            {
+                Console.WriteLine($"It will be a tough winter! More {Math.Floor(wineNeeded - wine)} liters wine needed.");
+            }
+            else
+            {
+                double WinePerWorker = (wine - wineNeeded) / workers;
+                Console.WriteLine($"Good harvest this year! Total wine: {Math.Floor(wine)} liters.");
+                Console.WriteLine($"{Math.Ceiling(wine - wineNeeded)} liters left -> {Math.Ceiling(WinePerWorker)} liters per person.");
+            }
+        }
+    }
+}
---
+++ Exercises/10 klas/Modul #1 Uvod v programiraneto/sleepyTom/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace sleepyTom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int freeDays = int.Parse(Console.ReadLine());
+            int workingDays = 365 - freeDays;
+            int playTime = workingDays * 63 + freeDays * 127;
+
+            int difference = playTime - 30000;
+            int hours = Math.Abs(difference / 60);
+            int minutes = Math.Abs(difference % 60);
+            
+            if (playTime > 30000)
+            {
+                Console.WriteLine("Tom will run away");
+                Console.WriteLine($"{hours} hours and {minutes} minutes more for play");
+            }
+            else
+            {
+                Console.WriteLine("Tom sleeps well");
+                Console.WriteLine($"{hours} hours and {minutes} minutes less for play");
+            }
+
+        }
+    }
+}
