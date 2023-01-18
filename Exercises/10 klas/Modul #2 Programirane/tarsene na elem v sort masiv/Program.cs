64d4ec45ec4c6309e13a8a4252769227a7d73ba3
Kris040805 <113238706+Kris040805@users.noreply.github.com>
2023-01-18 21:16:24 +0200

    Add files via upload

---
+++ Exercises/10 klas/Modul #2 Programirane/tarsene na elem v sort masiv/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace tarsene_na_elem_v_sort_masiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
+            bool found = false;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (n == arr[i])
+                {
+                    found = true;
+                    break;
+                }
+            }
+            Console.WriteLine(found ? "Yes" : "No");
+        }
+    }
+}
---
+++ Exercises/10 klas/Modul #2 Programirane/vmakvane na element v sort masiv/Program.cs
@@ -0,0 +1,30 @@
+﻿   using System;
+using System.Linq;
+
+namespace vmakvane_na_element_v_sort_masiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int number = int.Parse(Console.ReadLine());
+            int[] arr2 = new int[arr1.Length + 1];
+            int placed = 0;
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                if (number >= arr1[i - placed] || placed == 1)
+                {
+                    arr2[i] = arr1[i - placed];
+                }
+                else
+                {
+                    arr2[i] = number;
+                    placed = 1;
+                }
+            }
+            Console.WriteLine(string.Join(" ", arr2));
+        }
+    }
+}
