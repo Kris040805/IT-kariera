e0f7482d570301494141563dc9a0244d04df2f98
Kris040805 <113238706+Kris040805@users.noreply.github.com>
2023-01-18 17:07:25 +0200

    Add files via upload

---
+++ Exams/Exam module №1/winningNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace winningNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int a = 0; a <= 9; a++)
+                {
+                    for (int b = 0; b <= 9; b++)
+                    {
+                        for (int c = 0; c <= 9; c++)
+                        {
+                            if (i + c == N  &&  a % 2 != 0  &&  (b == 4  ||  b == 7))
+                            {
+                                Console.Write($"{i}{a}{b}{c} ");
+                            }                                                
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
