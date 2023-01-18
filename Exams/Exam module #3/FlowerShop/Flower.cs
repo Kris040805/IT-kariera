110a3824ed50a6ef1412d486fdde90e66e17c5de
Kris040805 <113238706+Kris040805@users.noreply.github.com>
2023-01-18 17:15:13 +0200

    Add files via upload

---
+++ Exams/Exam module #3/FlowerShop/Flower.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShop
+{
+    class Flower
+    {
+        public Flower(string type, string color, double price)
+        {
+            //TODO: Добавете вашия код тук …
+        }
+
+        public string Type
+        {
+
+        }
+
+        public string Color
+        {
+
+        }
+
+
+        public double Price
+        {
+
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException(); //премахнете този ред като добавите вашия код
+                                                 //TODO: Добавете вашия код тук …
+        }
+    }
+}
---
+++ Exams/Exam module #3/FlowerShop/FlowerStore.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShop
+{
+    class FlowerStore
+    {
+        public FlowerStore(string name)
+        {
+            //TODO: Добавете вашия код тук …
+        }
+
+        public string Name
+        {
+        }
+
+        public void AddFlower(Flower flower)
+        {
+            throw new NotImplementedException(); //премахнете този ред като добавите вашия код
+            //TODO: Добавете вашия код тук …
+        }
+
+        public bool SellFlower(Flower flower)
+        {
+            throw new NotImplementedException(); //премахнете този ред като добавите вашия код
+            //TODO: Добавете вашия код тук …
+        }
+
+        public double CalculateTotalPrice()
+        {
+            throw new NotImplementedException(); //премахнете този ред като добавите вашия код
+            //TODO: Добавете вашия код тук …
+        }
+
+        public Flower GetFlowerWithHighestPrice()
+        {
+            throw new NotImplementedException(); //премахнете този ред като добавите вашия код
+            //TODO: Добавете вашия код тук …
+        }
+
+        public Flower GetFlowerWithLowestPrice()
+        {
+            throw new NotImplementedException(); //премахнете този ред като добавите вашия код
+            //TODO: Добавете вашия код тук …
+        }
+
+        public void RenameFlowerStore(string newName)
+        {
+            throw new NotImplementedException(); //премахнете този ред като добавите вашия код
+            //TODO: Добавете вашия код тук …
+        }
+
+        public void SellAllFlowers()
+        {
+            throw new NotImplementedException(); //премахнете този ред като добавите вашия код
+            //TODO: Добавете вашия код тук …
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException(); //премахнете този ред като добавите вашия код
+            //TODO: Добавете вашия код тук …
+        }
+    }
+}
