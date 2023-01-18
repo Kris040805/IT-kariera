f78fb08259c3abf3d9931648e1fdf6cfdb567e2e
Kris040805 <113238706+Kris040805@users.noreply.github.com>
2023-01-18 17:14:25 +0200

    Add files via upload

---
+++ Exams/Exam module #3/brrr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace brrr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
