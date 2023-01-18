4c79c0e2faae4ef629f5379ab5b636f49a199880
Kris040805 <113238706+Kris040805@users.noreply.github.com>
2023-01-18 20:57:05 +0200

    Add files via upload

---
+++ Exercises/10 klas/Modul #1 Uvod v programiraneto/Histograma/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Histograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+            double p4 = 0;
+            double p5 = 0;
+            
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+
+                if (currentNum < 200)
+                {
+                    p1++;
+                }
+                else if (currentNum<400)
+                {
+                    p2++;
+                }
+                else if (currentNum < 600)
+                {
+                    p3++;
+                }
+                else if (currentNum < 800)
+                {
+                    p4++;
+                }
+                else p5++;
+            }
+            Console.WriteLine("---------------");
+            Console.WriteLine("{0:f2}%", p1 * 100 / n);
+            Console.WriteLine("{0:f2}%", p2 * 100 / n);
+            Console.WriteLine("{0:f2}%", p3 * 100 / n);
+            Console.WriteLine("{0:f2}%", p4 * 100 / n);
+            Console.WriteLine("{0:f2}%", p5 * 100 / n);
+        }
+    }
+}
---
+++ Exercises/10 klas/Modul #1 Uvod v programiraneto/hotelRoom/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace hotelRoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int nights = int.Parse(Console.ReadLine());
+
+            double studioPrice = 0;
+            double apartmentPrice = 0;
+
+            switch (month)
+            {
+                case "May":
+                case "October":
+
+                    studioPrice = 50;
+                    apartmentPrice = 65;
+
+                    if (nights > 14)
+                    {
+                        studioPrice = 50 - (50 * 0.3);
+                        apartmentPrice = 65 - (65 * 0.1);
+                    }
+                    else if (nights>7)
+                    {
+                        studioPrice = 50 - (50 * 0.05);
+                    }
+                    break;
+                case "June":
+                case "September":
+                    
+                    studioPrice = 75.20;
+                    apartmentPrice = 68.70;
+
+                    if (nights > 14)
+                    {
+                        studioPrice = 75.20 - (75.20 * 0.2);
+                        apartmentPrice = 68.70 - (68.70 * 0.1);
+                    }
+                    break;
+                case "July":
+                case "August":
+
+                    studioPrice = 76;
+                    apartmentPrice = 77;
+
+                    if (nights > 14)
+                    {
+                        apartmentPrice = 77 - (77 * 0.1);
+                    }
+                    
+                    break;
+
+                   
+            }
+
+            double totalApartmentPrice = apartmentPrice * nights;
+            double totalStudioPrice = studioPrice * nights;
+
+            Console.WriteLine($"Apartment: {totalApartmentPrice:F2} lv.");
+            Console.WriteLine($"Studio: {totalStudioPrice:F2} lv.");
+        }
+    }
+}
